9fdc446264f1bda9cade26a221d1342d83719b1e
nordomecode <dumpyreg@gmail.com>
2023-08-07 21:17:03 +1000

    Add team selector

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,6 +4,9 @@ import femaleProfile from './images/femaleProfile.jpg'
 import maleProfile from './images/maleProfile.jpg'
 
 const Employees = () => {
+
+    const [selectedTeam, setTeam] = useState('TeamA')
+
     const [employees, setEmployees] = useState([{
         id: 1,
         fullName: "Bob Jones",
@@ -89,15 +92,34 @@ const Employees = () => {
         teamName: "TeamD"
       }])
 
+
+    function handleTeamSelectionChange(event) {
+      setTeam(event.target.value)
+    }
+      
     return (
       <main className='container'>
+
+        <div className="row justify-content-center mt-3 mb-3" >
+          <div className="col-6">
+            <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
+              <option value='TeamA'>Team A</option>
+              <option value='TeamB'>Team B</option>
+              <option value='TeamC'>Team C</option>
+              <option value='TeamD'>Team D</option>
+            </select>
+          </div>
+        </div>
+
         <div className="row justify-content-center mt-3 mb-3" >
           <div className="col">
             <div className="card-collection">
               {
               employees.map((employee) => (
-                <div id={employee.id} className="card">
-                  <img src={femaleProfile} className='card-image-top'/>
+                <div id={employee.id} className="card m-2" style={{cursor: 'pointer'}}>
+                  {(employee.gender==='male') 
+                  ? <img src={maleProfile} className='card-image-top'/>
+                  : <img src={femaleProfile} className='card-image-top'/>}
                   <div className="card-body">
                     <h5 className='card-title'>
                       Full name: {employee.fullName}
