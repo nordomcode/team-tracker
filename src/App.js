e01c565ef76b79d1e020573b6f436e6a96e5f291
nordomecode <dumpyreg@gmail.com>
2023-08-08 14:20:16 +1000

    Add localStorage and useEfefct

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Header from './Header'
 import Footer from './Footer'
 import Employees from './Employees'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 
 
 function App() {
 
-  const [selectedTeam, setTeam] = useState('TeamA')
-  const [employees, setEmployees] = useState([{
+  const [selectedTeam, setTeam] = useState(JSON.parse(localStorage.getItem('selectedTeam')) || 'TeamA')
+  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) || [{
     id: 1,
     fullName: "Bob Jones",
     designation: "JavaScript Developer",
@@ -96,21 +96,30 @@ function App() {
   }])
 
 
-function handleTeamSelectionChange(event) {
-  setTeam(event.target.value)
-}
+  function handleTeamSelectionChange(event) {
+   setTeam(event.target.value)
+  }
 
-function handleEmployeeCardClick(event){
-  const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam) ? {...employee, teamName: ''} : {...employee, teamName: selectedTeam} : employee);
+  function handleEmployeeCardClick(event){
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam) ? {...employee, teamName: ''} : {...employee, teamName: selectedTeam} : employee);
 
   setEmployees(transformedEmployees)
-}
+  }
+
+  useEffect(() => {
+    localStorage.setItem('employeeList', JSON.stringify(employees))
+  }, [employees])
+
+  useEffect(() => {
+    localStorage.setItem('selectedTeam', JSON.stringify(selectedTeam))
+  }, [selectedTeam])
 
   return (
     <div className="App">
       <Header 
         selectedTeam={selectedTeam}
-        teamMemberCount = {employees.filter((employee) => employee.teamName===selectedTeam).length}/>
+        teamMemberCount = {employees.filter((employee) => employee.teamName===selectedTeam).length}
+      />
       <Employees 
         employees={employees}
         selectedTeam={selectedTeam}
